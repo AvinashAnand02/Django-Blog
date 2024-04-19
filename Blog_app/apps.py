79fadb83202f0d7f6db10197d8fc223fec36f13d
Avinash Anand <82769201+AvinashAnand02@users.noreply.github.com>
2024-04-19 22:16:47 +0530

    Update apps.py

--- Blog_app/apps.py
+++ Blog_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class BlogAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Blog_app'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
