70d9a9215e13ac5440f890ffefc6900871776ee1
Ike <dylanraspino264@gmail.com>
2024-05-25 21:21:31 +0000

    updated form and views.py to accept image in form

--- Blog_site/urls.py
+++ Blog_site/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Blog_app.urls')),
-    
-]
+    path('', include('Blog_app.urls')),   
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
